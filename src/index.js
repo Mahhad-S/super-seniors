cb57ea27b254baebd4dbf79ca2391c55e05a85e3
ethanyanez03 <ethanyanez66522@gmail.com>
2024-06-13 19:00:39 -0700

    Accpected commits

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const express=require("express")
-const app=express()
-const path=require("path")
-const hbs=require("hbs")
-const collection=require("./mongodb")
-
-const templatePath=path.join(__dirname,'../templates')
-const publicPath = path.join(__dirname, '../public')
-
-app.use(express.json())
-app.set("view engine","hbs")
-app.set("views",templatePath)
-app.use(express.urlencoded({extended:false}))
-app.use(express.static(publicPath)); // Serve static files
-
-=======
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
@@ -43,7 +26,6 @@ app.use(session({
     }),
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
 }));
->>>>>>> 9a876406d93f192e74f5a19d1a588c896fc590b7
 app.get("/",(req,res)=>{
     res.render("home")
 })
@@ -56,37 +38,6 @@ app.get("/login",(req,res)=>{
     res.render("login")
 })
 
-<<<<<<< HEAD
-app.get("/register",(req,res)=>{
-    res.render("register")
-})
-
-app.post("/register",async (req,res)=>{
-    const data={
-        name:req.body.name,
-        password:req.body.password
-    }
-
-    await  collection.insertMany([data])
-
-    res.render("home")
-})
-
-app.post("/login",async (req,res)=>{
-    const {name,password} = req.body
-    const user=await collection.findOne({ name, password })
-
-    if (user){
-        res.render("dashboard")
-    } else{
-        res.render("login", { error: "Username or Password Incorrect. Please register for an account if you have not already" })
-    }
-})
-
-app.listen(3000,()=>{
-    console.log("port connected")
-})
-=======
 // Middleware to check if user is authenticated
 function isAuthenticated(req, res, next) {
     if (req.session.user) {
@@ -155,4 +106,3 @@ app.get("/logout", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
->>>>>>> 9a876406d93f192e74f5a19d1a588c896fc590b7
